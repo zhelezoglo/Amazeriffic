430a6db2eda489ce3eed5e2b5837a1b7ef6baa0c
som <serhii.solomakha@gmail.com>
2014-06-30 18:31:04 +0300

    Adding 'Add' and 'Oldest' functionality

--- js/app.js
+++ js/app.js
@@ -14,27 +14,47 @@ var main = function () {
 	$(".tabs a span").toArray().forEach(function (element) {
 		$(element).on("click", function() {
 			var $element = $(element),
-				$content;
+				$content,
+				$input,
+				$button;
 			$(".tabs a span").removeClass("active");
 			$element.addClass("active");
 			$("main .content").empty();
 			if ($element.parent().is(":nth-child(1)")) {
-				console.log("FIRST TAB CLICKED!");
+				$content = $("<ul>");
+				var toDosReversed = Array.prototype.slice.call(toDos);
+				toDosReversed.reverse().forEach(function (todo) {
+					$content.append($("<li>").text(todo));
+				});
 			} else if ($element.parent().is(":nth-child(2)")) {
 				$content = $("<ul>");
 				toDos.forEach(function (todo) {
 					$content.append($("<li>").text(todo));
 				});
-				$("main .content").append($content);
 			} else if ($element.parent().is(":nth-child(3)")) {
-				console.log("THIRD TAB CLICKED!");
+                // input a new to-do
+                $input = $("<input>"),
+                $button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input).append($button);
+               /* Alternatively append() allows multiple arguments so the above
+                can be done with $content = $("<div>").append($input, $button); */
 			}
+
+			$("main .content").append($content);
 
 			return false;
 		});
 	});
 
-	// $(".tabs a:first-child span").trigger("click");
+	$(".tabs a:first-child span").trigger("click");
 
 };
 $(document).ready(main);
